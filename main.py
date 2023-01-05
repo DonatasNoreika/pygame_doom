1362e1d237dc13844445e985e04843407eebd51f
Donatas Noreika <donoras@gmail.com>
2023-01-05 18:39:57 +0200

    Pridėtas raycasting debug su bugu (reikia sutvarkyt

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 from settings import RES, FPS
 from map import Map
 from player import Player
+from raycasting import RayCasting
 
 class Game:
     def __init__(self):
@@ -16,9 +17,11 @@ class Game:
     def new_game(self):
         self.map = Map(self)
         self.player = Player(self)
+        self.raycasting = RayCasting(self)
 
     def update(self):
         self.player.update()
+        self.raycasting.update()
         pg.display.flip()
         self.delta_time = self.clock.tick(FPS)
         pg.display.set_caption(f"{self.clock.get_fps() :.1f}")
