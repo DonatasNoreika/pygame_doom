db4b451cd1c80029c4c9e0ece7d7b69ba926f323
Donatas Noreika <donoras@gmail.com>
2023-01-06 10:56:23 +0200

    Pridėtas žaidėjo valdymas pele ir nuimtas kursorius

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 
-from settings import PLAYER_POS, PLAYER_ANGLE, PLAYER_SPEED, PLAYER_ROT_SPEED, WIDTH, HEIGHT, PLAYER_SIZE_SCALE
+from settings import PLAYER_POS, PLAYER_ANGLE, PLAYER_SPEED, PLAYER_ROT_SPEED, WIDTH, HEIGHT, PLAYER_SIZE_SCALE, MOUSE_BORDER_LEFT, MOUSE_BORDER_RIGHT, HALF_WIDTH, HALF_HEIGHT, MOUSE_MAX_REL, MOUSE_SENSITIVITY
 import pygame as pg
 import math
 
@@ -60,8 +60,17 @@ class Player:
         pg.draw.circle(self.game.screen, 'green', (self.x * 100, self.y * 100), 15)
 
 
+    def mouse_control(self):
+        mx, my = pg.mouse.get_pos()
+        if mx < MOUSE_BORDER_LEFT or mx > MOUSE_BORDER_RIGHT:
+            pg.mouse.set_pos([HALF_WIDTH, HALF_HEIGHT])
+        self.rel = pg.mouse.get_rel()[0]
+        self.rel = max(-MOUSE_MAX_REL, min(MOUSE_MAX_REL, self.rel))
+        self.angle += self.rel * MOUSE_SENSITIVITY * self.game.delta_time
+
     def update(self):
         self.movement()
+        self.mouse_control()
 
     @property
     def pos(self):
