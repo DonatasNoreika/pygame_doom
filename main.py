904f76a389efda105d7be9a0d972cd2bacf59690
Donatas Noreika <donoras@gmail.com>
2023-01-06 13:16:20 +0200

    Dėjau kandeliabros spraitą, bet nesimato (bugas)

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from map import Map
 from player import Player
 from raycasting import RayCasting
 from object_renderer import ObjectRenderer
+from sprite_object import SpriteObject
 
 class Game:
     def __init__(self):
@@ -21,10 +22,12 @@ class Game:
         self.player = Player(self)
         self.object_renderer = ObjectRenderer(self)
         self.raycasting = RayCasting(self)
+        self.static_sprite = SpriteObject(self)
 
     def update(self):
         self.player.update()
         self.raycasting.update()
+        self.static_sprite.update()
         pg.display.flip()
         self.delta_time = self.clock.tick(FPS)
         pg.display.set_caption(f"{self.clock.get_fps() :.1f}")
